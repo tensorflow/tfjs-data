44561a63de9a126177b2dffa89754658b7b293f4
kangyizhang <kangyizhang@google.com>
2018-10-31 15:38:21 -0700

    remove materilized data and calculate baseline from dataset

--- demo/boston-housing/index.ts
+++ demo/boston-housing/index.ts
@@ -20,30 +20,37 @@ import {DataElement, Dataset} from '@tensorflow/tfjs-data/dist/src';
 // TODO(kangyi, soergel): Remove this once we have a public statistics API.
 import {computeDatasetStatistics, DatasetStatistics} from '@tensorflow/tfjs-data/dist/src/statistics';
 import * as tfl from '@tensorflow/tfjs-layers/dist/index';
+import {batchSetValue} from '@tensorflow/tfjs-layers/dist/variables';
 
 import {BostonHousingDataset} from './data';
 import * as ui from './ui';
 
 // Some hyperparameters for model training.
 const NUM_EPOCHS = 250;
-const BATCH_SIZE = 10;
+const BATCH_SIZE = 40;
+//  Total number of steps (batches of samples) before declaring one epoch
+//  finished and starting the next epoch. It should typically be equal to the
+//  number of samples of the dataset divided by the batch size, so that
+//  `fitDataset()` call can utilize the entire dataset. Here the train dataset
+//  has 285 samples and batchSize is 40, so `fitDataset()` should take 8 batches
+//  per epoch.
+const BATCHES_PER_EPOCH = 8;
+// Total number of batches of samples to draw from `validationData` for
+// validation purpose before stopping at the end of every epoch. Here the
+// validation dataset has 50 samples and batch size is 40, so
+// `validationDataset` should take 2 batchSetValue.
+const VALIDATION_PATCHES = 2;
 const LEARNING_RATE = 0.01;
 
 interface PreparedData {
-  normalizedTrainFeatures: tf.Tensor2D;
-  trainTarget: tf.Tensor2D;
-  normalizedTestFeatures: tf.Tensor2D;
-  testTarget: tf.Tensor2D;
   trainData: Dataset<DataElement>;
+  validationData: Dataset<DataElement>;
   testData: Dataset<DataElement>;
 }
 
 const preparedData: PreparedData = {
-  normalizedTrainFeatures: null,
-  trainTarget: null,
-  normalizedTestFeatures: null,
-  testTarget: null,
   trainData: null,
+  validationData: null,
   testData: null
 };
 
@@ -64,30 +71,15 @@ export async function loadDataAndNormalize() {
   stats = await computeDatasetStatistics(
       bostonData.trainDataset.map((row: Array<{key: number}>) => row[0]));
 
-  // Normalizes features data.
-  const normalizedTrainData = bostonData.trainDataset.map(normalizeFeatures);
-  const normalizedTestData = bostonData.testDataset.map(normalizeFeatures);
-
+  // Normalizes data.
   preparedData.trainData =
-      bostonData.trainDataset.map(normalizeFeatures).batch(40).repeat();
+      bostonData.trainDataset.map(normalizeFeatures).batch(BATCH_SIZE).repeat();
+  preparedData.validationData =
+      bostonData.validationDataset.map(normalizeFeatures)
+          .batch(BATCH_SIZE)
+          .repeat();
   preparedData.testData =
-      bostonData.testDataset.map(normalizeFeatures).batch(40).repeat();
-
-  // Materializes data into arrays. Following codes should be removed once
-  // model.fitDataset is available.
-  const trainIter = await normalizedTrainData.iterator();
-  const trainData = await trainIter.collect();
-  const testIter = await normalizedTestData.iterator();
-  const testData = await testIter.collect();
-
-  preparedData.normalizedTrainFeatures =
-      tf.tensor2d(trainData.map((row: number[][]) => row[0]));
-  preparedData.trainTarget =
-      tf.tensor2d(trainData.map((row: number[][]) => row[1]));
-  preparedData.normalizedTestFeatures =
-      tf.tensor2d(testData.map((row: number[][]) => row[0]));
-  preparedData.testTarget =
-      tf.tensor2d(testData.map((row: number[][]) => row[1]));
+      bostonData.testDataset.map(normalizeFeatures).batch(BATCH_SIZE).repeat();
 }
 
 /**
@@ -151,15 +143,10 @@ export const run = async (model: tfl.Sequential) => {
 
   await ui.updateStatus('Starting training process...');
   await model.fitDataset(preparedData.trainData, {
-    // batchSize: BATCH_SIZE,
     epochs: NUM_EPOCHS,
-    batchesPerEpoch: 1,
-    // validationSplit: 0.2,
-    // validationData: preparedData.testData,
-    // validationBatches: 1,
-    validationData:
-        [preparedData.normalizedTestFeatures, preparedData.testTarget],
-    validationBatchSize: BATCH_SIZE,
+    batchesPerEpoch: BATCHES_PER_EPOCH,
+    validationData: preparedData.validationData,
+    validationBatches: VALIDATION_PATCHES,
     callbacks: {
       onEpochEnd: async (epoch: number, logs) => {
         await ui.updateStatus(`Epoch ${epoch + 1} of ${NUM_EPOCHS} completed.`);
@@ -181,14 +168,36 @@ export const run = async (model: tfl.Sequential) => {
       `Test-set loss: ${testLoss.toFixed(4)}`);
 };
 
-export const computeBaseline = () => {
-  const avgPrice = tf.mean(preparedData.trainTarget);
-  console.log(`Average price: ${avgPrice.dataSync()}`);
-  const baseline =
-      tf.mean(tf.pow(tf.sub(preparedData.testTarget, avgPrice), 2));
-  console.log(`Baseline loss: ${baseline.dataSync()}`);
-  const baselineMsg = `Baseline loss (meanSquaredError) is ${
-      baseline.dataSync()[0].toFixed(2)}`;
+export const computeBaseline = async () => {
+  const trainIter = await bostonData.trainDataset.iterator();
+  let trainSum = 0;
+  let trainCount = 0;
+  while (true) {
+    const row: {done: boolean, value: number[]} =
+        (await trainIter.next()) as {done: boolean, value: number[]};
+    if (row.done) {
+      break;
+    }
+    trainSum += Number(row.value[1]);
+    trainCount++;
+  }
+  const trainMean = trainSum / (trainCount === 0 ? 1 : trainCount);
+
+  const testIter = await bostonData.testDataset.iterator();
+  let testSquareError = 0;
+  let testCount = 0;
+  while (true) {
+    const row: {done: boolean, value: number[]} =
+        (await testIter.next()) as {done: boolean, value: number[]};
+    if (row.done) {
+      break;
+    }
+    testSquareError += Math.pow(row.value[1] - trainMean, 2);
+    testCount++;
+  }
+  const baseline = testSquareError / (testCount === 0 ? 1 : testCount);
+  const baselineMsg =
+      `Baseline loss (meanSquaredError) is ${baseline.toFixed(2)}`;
   ui.updateBaselineStatus(baselineMsg);
 };
 
