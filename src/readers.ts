b2cbdb4fa01533319dc2015fb74a59af1839af63
kangyizhang <kangyizhang@google.com>
2018-12-19 14:00:04 -0800

    save

--- src/readers.ts
+++ src/readers.ts
@@ -16,9 +16,11 @@
  * =============================================================================
  */
 
+import {Dataset, datasetFromIteratorFn} from './dataset';
 import {CSVDataset} from './datasets/csv_dataset';
+import {iteratorFromFunction} from './iterators/lazy_iterator';
 import {URLDataSource} from './sources/url_data_source';
-import {CSVConfig} from './types';
+import {CSVConfig, DataElement} from './types';
 
 /**
  * Create a `CSVDataset` by reading and decoding CSV file(s) from provided URLs.
@@ -89,4 +91,76 @@ import {CSVConfig} from './types';
  */
 export function csv(source: string, csvConfig: CSVConfig = {}): CSVDataset {
   return new CSVDataset(new URLDataSource(source), csvConfig);
+}
+
+/**
+ * Create a `Dateset` by reading and decoding CSV file(s) from provided URLs.
+ *
+ * ```js
+ * const csvUrl =
+ * 'https://storage.googleapis.com/tfjs-examples/multivariate-linear-regression/data/boston-housing-train.csv';
+ *
+ * async function run() {
+ *   // We want to predict the column "medv", which represents a median value of
+ *   // a home (in $1000s), so we mark it as a label.
+ *   const csvDataset = tf.data.csv(
+ *     csvUrl, {
+ *       columnConfigs: {
+ *         medv: {
+ *           isLabel: true
+ *         }
+ *       }
+ *     });
+ *
+ *   // Number of features is the number of column names minus one for the label
+ *   // column.
+ *   const numOfFeatures = (await csvDataset.columnNames()).length - 1;
+ *
+ *   // Prepare the Dataset for training.
+ *   const flattenedDataset =
+ *     csvDataset
+ *     .map(([rawFeatures, rawLabel]) =>
+ *       // Convert rows from object form (keyed by column name) to array form.
+ *       [Object.values(rawFeatures), Object.values(rawLabel)])
+ *     .batch(10);
+ *
+ *   // Define the model.
+ *   const model = tf.sequential();
+ *   model.add(tf.layers.dense({
+ *     inputShape: [numOfFeatures],
+ *     units: 1
+ *   }));
+ *   model.compile({
+ *     optimizer: tf.train.sgd(0.000001),
+ *     loss: 'meanSquaredError'
+ *   });
+ *
+ *   // Fit the model using the prepared Dataset
+ *   return model.fitDataset(flattenedDataset, {
+ *     epochs: 10,
+ *     callbacks: {
+ *       onEpochEnd: async (epoch, logs) => {
+ *         console.log(epoch + ':' + logs.loss);
+ *       }
+ *     }
+ *   });
+ * }
+ *
+ * await run();
+ * ```
+ *
+ * @param source URL to fetch CSV file.
+ * @param csvConfig (Optional) A CSVConfig object that contains configurations
+ *     of reading and decoding from CSV file(s).
+ */
+/** @doc {
+ *   heading: 'Data',
+ *   subheading: 'Creation',
+ *   namespace: 'data',
+ *   configParamIndices: [1]
+ *  }
+ */
+export function generator<T extends DataElement>(f: () =>IteratorResult<T>| Promise<IteratorResult<T>>): Dataset<T> {
+    const iter = iteratorFromFunction(f);
+  return datasetFromIteratorFn(iter);
 }
