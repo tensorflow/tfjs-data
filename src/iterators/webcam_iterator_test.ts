cb4ed9bd40ba60117f9a1f8eb548ded271385722
kangyizhang <kangyizhang@google.com>
2019-03-18 10:43:27 -0700

    save

--- src/iterators/webcam_iterator_test.ts
+++ src/iterators/webcam_iterator_test.ts
@@ -202,34 +202,16 @@ describeWithFlags('WebcamIterator', WEBGL_ENVS, () => {
     // console.log(canvasElement);
   });
 
-  it('creates webcamIterator with no html element',
-     async () => {
-         // const image = document.createElement('img');
-         // image.src = 'image.jpeg';
-         // const canvas = document.createElement('canvas');
-         // const ctx = canvas.getContext('2d');
-         // ctx.drawImage(image, 0, 0);
-         // navigator.mediaDevices.getUserMedia = async () => {
-         //   // await new Promise(resolve => setTimeout(() => {
-         //   //                     videoElement.dispatchEvent(
-         //   //                         new Event('loadedmetadata'));
-         //   //                     resolve();
-         //   //                   }, 2000));
-         //   // tslint:disable-next-line:no-any
-         //   return (canvas as any).captureStream();
-         //   // const stream = new MediaStream([new MediaStreamTrack()]);
-         //   // // stream.addTrack(new MediaStreamTrack());
-         //   // return stream;
-         // };
+  it('creates webcamIterator with no html element', async () => {
+    navigator.mediaDevices.getUserMedia = async () => {
+      return stream;
+    };
 
-         // const videoElement = document.createElement('video');
-         // videoElement.width = 100;
-         // videoElement.height = 100;
-
-         // // await new Promise(resolve => setTimeout(resolve, 2000));
-         // const webcamIterator = await WebcamIterator.create(videoElement);
-         // const result = await webcamIterator.capture();
-         // expect(result.shape).toEqual([100, 100, 3]);
-         // // console.log(result);
-     });
+    // await new Promise(resolve => setTimeout(resolve, 2000));
+    const webcamIterator =
+        await WebcamIterator.create(null, {width: 100, height: 100});
+    const result = await webcamIterator.capture();
+    expect(result.shape).toEqual([100, 100, 3]);
+    // console.log(result);
+  });
 });
